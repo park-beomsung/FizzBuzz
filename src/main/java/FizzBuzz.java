96aff274856303bb9e08b7d59ac03d0ca7040731
park-beomsung <p.beomsung@fourm.co.jp>
2024-06-17 14:53:26 +0900

    Step3

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -7,6 +7,9 @@ public class FizzBuzz {
 			if(i % 3 == 0) {
 				System.out.println("Fizz");
 			}
+			else if(i % 5 == 0) {
+				System.out.println("Buzz");
+			}
 			else {
 				System.out.println(i);
 			}
